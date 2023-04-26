ba13da5c2712704847f14e02d5a86d9ecd9e3edc
Grischa Erbe <46897060+grischaerbe@users.noreply.github.com>
2023-04-26 10:54:12 +0200

    Better buttons

--- src/components/TimeAttack/UI/Key.svelte
+++ src/components/TimeAttack/UI/Key.svelte
@@ -1,12 +1,12 @@
 <div class=" inline-block text-gray-700 my-[4px] relative !p-0">
 	<div
-		class="absolute w-full h-full top-[0px] left-[0px] border-[8px] border-t-gray-400 border-l-gray-500 border-b-gray-700/95 border-r-gray-800/75 rounded-[6px] -z-1 box-border bg-white"
+		class="absolute w-full h-full top-[0px] left-[0px] border-[12px] border-t-[10px] border-t-gray-400 border-l-gray-500 border-b-gray-700/95 border-r-gray-800/75 rounded-[6px] -z-1 box-border bg-white"
 	/>
 	<div
 		class="absolute top-0 left-0 w-full h-full border border-r-gray-900/70 border-l-gray-700/70 border-t-gray-600/70 border-b-gray-900/70 z-10 rounded-[6px]"
 	/>
 	<div
-		class="border border-gray-300/50 rounded-[4px] bg-gradient-to-b from-gray-400 to-gray-300 via-gray-300 relative px-[5px] py-0 m-[4px] text-center min-w-[1.6em]"
+		class="border border-gray-300/50 border-b-gray-200 rounded-[4px] bg-gradient-to-b from-gray-400 to-gray-300 via-gray-300 relative px-[8px] !py-[5px] m-[6px] mt-[4px] text-center min-w-[1.6em] text-[0.85em]"
 	>
 		<slot />
 	</div>
