3ae0fcabfdec7184f24d9a68e93477d76952d01f
Grischa Erbe <grischa.erbe@googlemail.com>
2023-11-08 00:40:06 +0100

    cleanup

--- src/routes/(env)/(endless-race)/menu/main/+page.svelte
+++ src/routes/(env)/(endless-race)/menu/main/+page.svelte
@@ -3,47 +3,9 @@
 	import SpecialButton from '../../../../../components/UI/components/SpecialButton.svelte'
 	import { Nakama } from '../../../../../lib/nakama/Nakama'
 	import { SessionManager } from '../../../../../lib/nakama/SessionManager'
-
-	const onClick = () => {
-		const img = new Image()
-		img.src = '/android-chrome-512x512.png'
-		img.onload = async () => {
-			const res = await Nakama.client.rpc(await SessionManager.getSession(), 'upload_image', {})
-			if (res) {
-				const url = (res.payload as any).url
-
-				// upload image to s3 presigned url as Blob
-				// make blob from image
-				const blob = await new Promise<Blob>((resolve, reject) => {
-					const xhr = new XMLHttpRequest()
-					xhr.open('GET', img.src)
-					xhr.responseType = 'blob'
-					xhr.onload = () => {
-						resolve(xhr.response)
-					}
-					xhr.onerror = () => {
-						reject(new TypeError('Local request failed'))
-					}
-					xhr.send()
-				})
-
-				// upload blob to s3
-				const res2 = await fetch(url, {
-					method: 'PUT',
-					body: blob,
-					headers: {
-						'Content-Disposition': 'inline; filename="android-chrome-512x512.png',
-						'Content-Type': 'image/png'
-					}
-				})
-			}
-		}
-	}
 </script>
 
 <BottomBar>
-	<SpecialButton slot="left" on:click={onClick}>TEST</SpecialButton>
-
 	<SpecialButton style="inverted" href="https://legrisch.com" class="text-[0.5em]" slot="right">
 		Made by Grischa Erbe
 	</SpecialButton>
