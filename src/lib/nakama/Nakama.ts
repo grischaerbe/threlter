a36b37f23b5bca301894c608ba048530d4ee1a9e
Grischa Erbe <grischa.erbe@googlemail.com>
2023-11-08 18:41:59 +0100

    moved to static env

--- src/lib/nakama/Nakama.ts
+++ src/lib/nakama/Nakama.ts
@@ -1,12 +1,17 @@
-import { env } from '$env/dynamic/public'
+import {
+	PUBLIC_NAKAMA_HOST,
+	PUBLIC_NAKAMA_PORT,
+	PUBLIC_NAKAMA_SERVER_KEY,
+	PUBLIC_NAKAMA_USE_SSL
+} from '$env/static/public'
 import { Client } from '@heroiclabs/nakama-js'
 
 export class Nakama {
 	public static client = new Client(
-		env.PUBLIC_NAKAMA_SERVER_KEY,
-		env.PUBLIC_NAKAMA_HOST,
-		env.PUBLIC_NAKAMA_PORT && env.PUBLIC_NAKAMA_PORT.length ? env.PUBLIC_NAKAMA_PORT : '',
-		env.PUBLIC_NAKAMA_USE_SSL === 'true' || env.PUBLIC_NAKAMA_USE_SSL === '1',
+		PUBLIC_NAKAMA_SERVER_KEY,
+		PUBLIC_NAKAMA_HOST,
+		PUBLIC_NAKAMA_PORT && PUBLIC_NAKAMA_PORT.length ? PUBLIC_NAKAMA_PORT : '',
+		PUBLIC_NAKAMA_USE_SSL === 'true' || PUBLIC_NAKAMA_USE_SSL === '1',
 		10e3,
 		true
 	)
