348e4b455f53cfe571857d27e97790253c818d79
Grischa Erbe <grischa.erbe@googlemail.com>
2023-11-08 18:37:30 +0100

    Added env vars

--- src/lib/nakama/Nakama.ts
+++ src/lib/nakama/Nakama.ts
@@ -1,5 +1,13 @@
+import { env } from '$env/dynamic/public'
 import { Client } from '@heroiclabs/nakama-js'
 
 export class Nakama {
-	public static client = new Client('defaultkey', '127.0.0.1', '7350')
+	public static client = new Client(
+		env.PUBLIC_NAKAMA_SERVER_KEY,
+		env.PUBLIC_NAKAMA_HOST,
+		env.PUBLIC_NAKAMA_PORT && env.PUBLIC_NAKAMA_PORT.length ? env.PUBLIC_NAKAMA_PORT : '',
+		env.PUBLIC_NAKAMA_USE_SSL === 'true' || env.PUBLIC_NAKAMA_USE_SSL === '1',
+		10e3,
+		true
+	)
 }
